ee7e5cb50d23ec480aa66f26e776a053efb91aff
Mohammad Murad <muhammad.talal.murad@gmail.com>
2024-10-13 15:37:34 -0700

    form added

--- src/components/helpers/form.js
+++ src/components/helpers/form.js
@@ -12,8 +12,45 @@
   }
   ```
 */
+"use client";
+
+import { useState } from "react";
 
 export default function Form() {
+  const [firstname, setFirstname] = useState("");
+
+  //   const [middlename, setMiddlename] = useState("");
+  //   const [lastname, setLastname] = useState("");
+  //   const [description, setDescription] = useState("");
+  //   const [date, setDate] = useState("");
+  //   const [isMale, setIsMale] = useState(false);
+
+  const [formData, setFormData] = useState({
+    firstname: "",
+    middlename: "",
+    lastname: "",
+    description: "",
+    email: "",
+    date: "",
+    gender: false,
+  });
+
+  const handleChange = (e) => {
+    const { name, type, checked, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: type === 'checkbox' ? checked : value,
+    });
+  };
+
+  function submitHadler(e) {
+    e.preventDefault();
+
+    console.log("Form data submitted:", formData);
+
+    // onCreatePerson(PersonData);
+  }
+
   return (
     <form>
       <div className="space-y-12">
@@ -28,33 +65,37 @@ export default function Form() {
           <div className="mt-4 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-2">
               <label
-                htmlFor="first-name"
+                htmlFor="firstname"
                 className="block text-sm font-medium leading-6 text-gray-900"
               >
                 First name
               </label>
               <div className="mt-2">
                 <input
-                  id="first-name"
-                  name="first-name"
+                  id="firstname"
+                  name="firstname"
                   type="text"
-                  autoComplete="given-name"
+                  value={formData.firstname}
+                  onChange={handleChange}
+                  autoComplete="first-name"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
             <div className="sm:col-span-2">
               <label
-                htmlFor="first-name"
+                htmlFor="middlename"
                 className="block text-sm font-medium leading-6 text-gray-900"
               >
                 Middle name
               </label>
               <div className="mt-2">
                 <input
-                  id="first-name"
-                  name="first-name"
+                  id="middlename"
+                  name="middlename"
                   type="text"
+                  value={formData.middlename}
+                  onChange={handleChange}
                   autoComplete="given-name"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
@@ -63,23 +104,46 @@ export default function Form() {
 
             <div className="sm:col-span-2">
               <label
-                htmlFor="last-name"
+                htmlFor="lastname"
                 className="block text-sm font-medium leading-6 text-gray-900"
               >
-                Email
+                Last name
               </label>
               <div className="mt-2">
                 <input
-                  id="last-name"
-                  name="last-name"
-                  type="text"
+                  id="lastname"
+                  name="lastname"
+                  type="mail"
+                  value={formData.lastname}
+                  onChange={handleChange}
                   autoComplete="family-name"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
           </div>
-          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+          <div className="mt-4 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-1">
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  autoComplete="email"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-1">
             <div className="sm:col-span-3">
               <label
                 htmlFor="first-name"
@@ -88,16 +152,19 @@ export default function Form() {
                 Description
               </label>
               <div className="mt-2">
-                <input
-                  id="first-name"
-                  name="first-name"
-                  type="text"
-                  autoComplete="given-name"
+                <textarea
+                  id="description"
+                  name="description"
+                  rows={3}
+                  value={formData.description}
+                  onChange={handleChange}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  defaultValue={""}
                 />
               </div>
             </div>
-
+          </div>
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-3">
               <label
                 htmlFor="last-name"
@@ -107,30 +174,32 @@ export default function Form() {
               </label>
               <div className="mt-2">
                 <input
-                  id="last-name"
-                  name="last-name"
-                  type="text"
-                  autoComplete="family-name"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  id="date"
+                  name="date"
+                  type="date"
+                  value={formData.date}
+                  onChange={handleChange}
+                  className="block w-full p-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
           </div>
-          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-            <div className="sm:col-span-3">
+          <div className="mt-10 grid grid-cols-2 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="flex row space-x-4">
               <label
                 htmlFor="first-name"
                 className="block text-sm font-medium leading-6 text-gray-900"
               >
-                checkbox for gender (is male)
+                Is Male
               </label>
-              <div className="mt-2">
+              <div>
                 <input
-                  id="first-name"
-                  name="first-name"
-                  type="text"
-                  autoComplete="given-name"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  id="gender"
+                  name="gender"
+                  type="checkbox"
+                  checked={formData.gender}
+                  onChange={handleChange}
+                  className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
                 />
               </div>
             </div>
@@ -140,7 +209,7 @@ export default function Form() {
 
       <div className="mt-6 flex items-center justify-end gap-x-6">
         <button
-          type="submit"
+          onClick={submitHadler}
           className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
           Create
